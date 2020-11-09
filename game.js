5917c8aaaaaaf140b3d9b3bfa1ea5f4421bd43e0
Adriana Simiand <asimiand@gmail.com>
2020-11-08 21:01:43 -0300

    01.03

--- game.js
+++ game.js
@@ -1,56 +1,16 @@
-var canvas = null,
+var KEY_ENTER = 13,
+    KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40,
+    
+    canvas = null,
     ctx = null,
+    lastPress = null,
+    pause = true,
     x = 50,
-    y = 50;
-
-window.requestAnimationFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 17);
-        };
-}());
-
-//Limpiamos la pantalla pintando un cuadrado arriba
-function paint(ctx) {
-    //Color del fondo
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
-    //Color del cosito que se mueve
-    ctx.fillStyle = '#0f0';
-    ctx.fillRect(x, y, 10, 10);
-}
-
-function act() {
-    x += 2;
-    if (x > canvas.width) {
-        x = 0;
-    }
-}
-
-//Llama a la funcion paint y act una y otra vez 
-function run() {
-    window.requestAnimationFrame(run);
-    act();
-    paint(ctx);
-}
-
-function init() {
-    canvas = document.getElementById('canvas');
-    ctx = canvas.getContext('2d');
-    
-    run();
-}
-
-window.addEventListener('load', init, false);
-
-
-
-
-
-
+    y = 50,
+    dir = 0;
 
 //Por problemas de compatibilidad ya que algunos navegadores no soportan el requestAnimationFrame
 window.requestAnimationFrame = (function () {
@@ -61,3 +21,111 @@ window.requestAnimationFrame = (function () {
             window.setTimeout(callback, 17);
         };
 }());
+
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.which;
+}, false);
+
+//Limpiamos la pantalla pintando un cuadrado arriba
+function paint(ctx) {
+    //Color del fondo
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    //Color del cosito que se mueve
+    ctx.fillStyle = '#0f0';
+    ctx.fillRect(x, y, 10, 10);
+
+    // Debug last key pressed
+    ctx.fillStyle = '#fff';
+    //ctx.fillText('Last Press: ' + lastPress, 0, 20);
+    
+    // Draw pause
+    if (pause) {
+        ctx.textAlign = 'center';
+        ctx.fillText('PAUSE', 150, 75);
+        ctx.textAlign = 'left';
+    }
+}
+
+function act() {
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+}
+
+function act() {
+    if (!pause) {
+        // Change Direction
+        if (lastPress == KEY_UP) {
+            dir = 0;
+        }
+        if (lastPress == KEY_RIGHT) {
+            dir = 1;
+        }
+        if (lastPress == KEY_DOWN) {
+            dir = 2;
+        }
+        if (lastPress == KEY_LEFT) {
+            dir = 3;
+        }
+
+        // Move Rect
+        if (dir == 0) {
+            y -= 10;
+        }
+        if (dir == 1) {
+            x += 10;
+        }
+        if (dir == 2) {
+            y += 10;
+        }
+        if (dir == 3) {
+            x -= 10;
+        }
+
+        // Out Screen
+        if (x > canvas.width) {
+            x = 0;
+        }
+        if (y > canvas.height) {
+            y = 0;
+        }
+        if (x < 0) {
+            x = canvas.width;
+        }
+        if (y < 0) {
+            y = canvas.height;
+        }
+    }
+    
+    // Pause/Unpause
+    if (lastPress == KEY_ENTER) {
+        pause = !pause;
+        lastPress = null;
+    }
+}
+
+function repaint() {
+    window.requestAnimationFrame(repaint);
+    paint(ctx);
+}
+
+//llama a la funcion run una y otra vez
+function run() {
+    setTimeout(run, 50);
+    act();
+}
+
+function init() {
+    // Get canvas and context
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    
+    // Start game
+    run();
+    repaint();
+}
+
+window.addEventListener('load', init, false);
